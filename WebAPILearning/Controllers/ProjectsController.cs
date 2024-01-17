4db2d47a08ba519ab04cdbaebf6004440ccf69e1
dorijantomic <dorijan11@gmail.com>
2024-01-17 19:26:49 +0100

    implement Tickets and projects controller

---
+++ WebAPILearning/Controllers/ProjectsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPILearning.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProjectsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Retrieved all of the projects.");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok($"Retrieved the project {id}");
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok("Successfully added a project.");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id)
+        {
+            return Ok($"Successfully updated project #{id}.");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok($"Successfully deleted project #{id}.");
+        }
+    }
+}
---
+++ WebAPILearning/Controllers/TicketsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPILearning.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TicketsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Reading all the tickets");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok($"Reading ticket #{id}");
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok("Creating a ticket");
+        }
+
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok("Updating a ticket");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok($"Deleting a ticket #{id}");
+        }
+    }
+}
