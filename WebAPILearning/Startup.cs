da82b49df4bb08e4225811571e392bd03ef9d056
dorijantomic <dorijan11@gmail.com>
2024-02-07 20:13:53 +0100

    add version 2

--- WebAPILearning/Startup.cs
+++ WebAPILearning/Startup.cs
@@ -40,7 +40,11 @@ namespace WebAPILearning
                 //options.ApiVersionReader = new HeaderApiVersionReader("X-API-Version");
             });
             services.AddVersionedApiExplorer(options => options.GroupNameFormat = "'v'VVV");
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "My Web API v1", Version = "version 1" });
+                options.SwaggerDoc("v2", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "My Web API v2", Version = "version 2" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -60,6 +64,7 @@ namespace WebAPILearning
                     options =>
                     {
                         options.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1");
+                        options.SwaggerEndpoint("/swagger/v2/swagger.json", "WebAPI v2");
                     }
                     );
             }
